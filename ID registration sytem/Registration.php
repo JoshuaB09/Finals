81778d48948057d75fe5db06ecd728f617e51ec6
JoshuaB09 <JoshuaB09@gmail.com>
2022-10-20 06:49:56 +0800

    initial upload

--- ID registration sytem/Registration.php
+++ ID registration sytem/Registration.php
@@ -1,40 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ID Registration System</title>
+	<title>Registration</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
 <body>
 
 <div>
-	<form action="Registration.php" method="post">
+	<form action="RegistrationData.php" method="post">
 		<div class="container">
+		<div class="login-box">
+		<div class="row">
+		<div class="col-md-6">
 			<h2>Registration form</h2>
-			<label for="IDN"><b>ID Number</b></label>
+			<label for="IDN"><b>ID Number</b></label><br>
 			<input type="text" name="ID Number" required><br>
 
-			<label for="LName"><b>Last Name</b></label>
+			<label for="LName"><b>Last Name</b></label><br>
 			<input type="text" name="Lname" required><br>
 
-			<label for="Fname"><b>First Name</b></label>
+			<label for="Fname"><b>First Name</b></label><br>
 			<input type="text" name="Fname" required><br>
 
-			<label for="Mname"><b>Middle Name</b></label>
+			<label for="Mname"><b>Middle Name</b></label><br>
 			<input type="text" name="Mname" required><br>
 
-  			<label for="bdaymonth"><b>Birthday:</b></label>
+  			<label for="bdaymonth"><b>Birthday:</b></label><br>
  			<input type="month" id="bdaymonth" name="bdaymonth"><br>
 
- 			<h3> Parent Names</h3><br>
-
-			<label for="Nfather"><b>Name of Father</b></label>
+ 			<h4> Parent Names</h4>
+			<label for="Nfather"><b>Name of Father</b></label><br>
 			<input type="text" name="Nfather" required><br>
 
-			<label for="Nmother"><b>Name of Mother</b></label>
-			<input type="text" name="Nmother" required><br>
-			<input type="submit" name="Submit" value="Submit">
-		</div>	
+			<label for="Nmother"><b>Name of Mother</b></label><br>
+			<input type="text" name="Nmother" required><br>		<input type="submit" name="Submit" value="Submit"><br><br>
+			<a href="Login.php" style="color: black;">Click to Login</a>
 		</form>
-
+	</div>	
+	</div>
+	</div>
+	</div>
 
 
 
